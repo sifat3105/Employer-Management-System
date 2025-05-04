cca2143cf4bb61b7f13fbf456bb4c852f9e05e6d
MD SIFAT ALI <162755505+sifat3105@users.noreply.github.com>
2025-05-04 06:58:33 +0600

    Update settings.py

--- project_root/settings.py
+++ project_root/settings.py
@@ -44,6 +44,13 @@ INSTALLED_APPS = [
 
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
